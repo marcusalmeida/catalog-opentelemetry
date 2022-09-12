c95f424d8b3662d2140ef7f73fba559b6eafed78
Marcus Almeida <marcusmage@gmail.com>
2022-09-12 17:19:41 -0300

    Adicionando schema e data iniciais

---
+++ catalog/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO PRODUCT(ID, NAME, DESCRIPTION)
+VALUES 
+    (1, 'Stickers pack', 'A pack of rad stickers to display on your laptop or wherever you feel like.'),
+    (2, 'Lapel pin', 'With this lapel pin, support your favorite API Gateway and let everybody know about it.'),
+    (3, 'Tee-Shirt', 'The classic geek product! At a conference, at home, at work, this tee-shirt will be your best friend.');
